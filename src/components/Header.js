f2537c3b19d53a07ef0f0676dc0b8698617782b5
timlee0119 <t0966510608@gmail.com>
2019-01-02 23:35:35 +0800

    fix z-index and change to 7-tab navbar

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import { NavLink, Route } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faHome } from '@fortawesome/free-solid-svg-icons';
-
-const ResultNav = () => (
-  <div className="header">
-    <NavLink className="header__btn" activeClassName="header__btn--active" to="/result/background">受試者背景</NavLink>
-    <NavLink className="header__btn" activeClassName="header__btn--active" to="/result/res-anlys">實驗結果分析</NavLink>
-    <NavLink className="header__btn" activeClassName="header__btn--active" to="/result/ques-anlys">測後問卷分析</NavLink>
-    <NavLink className="header__btn" activeClassName="header__btn--active" to="/result/conclusion">結論</NavLink>
-  </div>
-);
 
 const Header = () => (
   <div>
@@ -20,11 +11,16 @@ const Header = () => (
       </NavLink>
       <NavLink className="header__btn" activeClassName="header__btn--active" to="/project">計劃緣起</NavLink> 
       <NavLink className="header__btn" activeClassName="header__btn--active" to="/method">研究方法</NavLink>
-      <NavLink className="header__btn" activeClassName="header__btn--active" to="/result">現階段成果</NavLink>
-      <NavLink className="header__btn" activeClassName="header__btn--active" to="/contact">聯絡我們</NavLink>
-      {/* <a href="javascript:void(0)" className="header__toggle" onClick="toggle()"></a> */}
+      <NavLink className="header__btn" activeClassName="header__btn--active" to="/result/background">受試者背景</NavLink>
+      <NavLink className="header__btn" activeClassName="header__btn--active" to="/result/res-anlys">實驗結果分析</NavLink>
     </div>
-    <Route path="/result" component={ResultNav}></Route>
+    <div className="header--nopt">
+      <FontAwesomeIcon icon={faHome} size="2x" style={{visibility: "hidden"}}/>
+      <NavLink className="header__btn" activeClassName="header__btn--active" to="/result/ques-anlys">測後問卷分析</NavLink>
+      <NavLink className="header__btn" activeClassName="header__btn--active" to="/result/conclusion">結論</NavLink>
+      <NavLink className="header__btn" activeClassName="header__btn--active" to="/contact">聯絡我們</NavLink>
+      <div className="header__btn header__btn--disable"></div>
+    </div>
   </div>
 );
 
